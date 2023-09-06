93c269bb1ca8516d4cec4392b3da807ed48747fc
Akanksha25052002 <akankshashrivastava2022@vitbhopal.ac.in>
2023-09-06 23:28:58 +0530

    add

--- java/Pattern.java
+++ java/Pattern.java
@@ -1,11 +1,23 @@
 public class Pattern{
 public static void main(String[] args) {
-    int r=5;
-    for(int i=1;i<=r;i++){
-        for(int j=1;j<=i;j++){
-            System.out.print("*");
+    // int r=5;
+    // for(int i=1;i<=r;i++){
+    //     for(int j=1;j<=i;j++){
+    //         System.out.print("*");
+    //     }
+    //      System.out.println();
+    // }
+
+     for(i=1;i<=10;i++){
+            for(j=1;j<=i;j++){
+                // document.getElementById("p1").innerText+="*";
+                console.log("*");
+                for(k=i;k>0;k--){
+                    console.log(" ");
+                }
+            }
+            0.
+        System.out.println(" ");
         }
-         System.out.println();
-    }
 }
 }
